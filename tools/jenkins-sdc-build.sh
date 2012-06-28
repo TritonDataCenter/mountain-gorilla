4be0d7d8f68d14b8244e8d2f212abb7182d16f41
Trent Mick <trentm@gmail.com>
2012-06-28 16:56:51 -0700

    update this jenkins build script to be general to sdc and the other MG-based tools

--- tools/jenkins-sdc-build.sh
+++ tools/jenkins-sdc-build.sh
@@ -1,39 +1,94 @@
 #!/bin/bash
 #
-# This script lives at
-# <mountain-gorilla.git/tools/jenkins-sdc-build.sh>.
+# This script lives at <mountain-gorilla.git/tools/jenkins-sdc-build.sh>.
+# A suggested jenkins build step for an MG component or full SDC build.
 #
-# A suggested jenkins build step for an MG full SDC build.
-# See the jenkins project for the *actual* current build steps:
-#   https://jenkins.joyent.us/job/sdc/configure
+# See the appropriate jenkins job for the *actual* current build steps:
+#   https://jenkins.joyent.us/job/$JOB/configure
 #
 
 set -o errexit
+unset LD_LIBRARY_PATH   # ensure don't get Java's libs (see OS-703)
 
+
+echo ""
+echo "#----------------------"
+start_time=$(date +%s)
+last_time=${start_time}
+
+rm -rf MG.last
 # Poorman's backup of last build run.
-rm -rf mountain-gorilla.last
-mv mountain-gorilla mountain-gorilla.last
+mkdir -p MG && mv MG MG.last
+rm -rf MG
+git clone git@git.joyent.com:mountain-gorilla.git MG
+cd MG
 
-git clone git@git.joyent.com:mountain-gorilla.git
-cd mountain-gorilla
+now_time=$(date +%s)
+elapsed=$((${now_time} - ${last_time}))
+last_time=${now_time}
+echo "TIME: clone MG took ${elapsed} seconds"
 
 LOG=build.log
 touch $LOG
 exec > >(tee ${LOG}) 2>&1
+
+
+
+echo ""
+echo "#---------------------- env"
 
 date
 pwd
 whoami
 env
 
+
+
 echo ""
-echo "#----------------------"
+echo "#---------------------- configure"
+
 [[ -z "$BRANCH" ]] && BRANCH=master
-./configure -b $BRANCH
+# Note the "-c" to use a cache dir one up, i.e. shared between builds of this job.
+CACHE_DIR=$(cd ../; pwd)/cache
+if [[ "$CLEAN_CACHE" == "true" ]]; then
+    rm -rf $CACHE_DIR
+fi
+if [[ "$JOB_NAME" == "sdc" ]]; then
+    TRACE=1 ./configure -c "$CACHE_DIR" -b "$BRANCH" -B "$TRY_BRANCH"
+else
+    TRACE=1 ./configure -t $JOB_NAME -c "$CACHE_DIR" -b "$BRANCH" -B "$TRY_BRANCH"
+fi
+
+now_time=$(date +%s)
+elapsed=$((${now_time} - ${last_time}))
+last_time=${now_time}
+echo "TIME: MG configure took ${elapsed} seconds"
+
+
 
 echo ""
-echo "#----------------------"
-gmake
+echo "#---------------------- make"
 
-cp $LOG bits/
+if [[ "$JOB_NAME" == "sdc" ]]; then
+    gmake
+else
+    gmake $JOB_NAME
+fi
+
+now_time=$(date +%s)
+elapsed=$((${now_time} - ${last_time}))
+last_time=${now_time}
+echo "TIME: build took ${elapsed} seconds"
+
+
+
+echo ""
+echo "#---------------------- upload"
+
+cp $LOG bits/$JOB_NAME/
 gmake upload_jenkins
+
+now_time=$(date +%s)
+elapsed=$((${now_time} - ${last_time}))
+last_time=${now_time}
+echo "TIME: upload took ${elapsed} seconds"
