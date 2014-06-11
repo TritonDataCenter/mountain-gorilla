05bda698b98544cde30a85a354d9fa73b0260847
Orlando Vazquez <ovazquez@gmail.com>
2014-06-11 07:44:46 +0000

    RELENG-529: work-around DEPTH_ZERO_SELF_SIGNED_CERT error

--- tools/jenkins/sdc-jenkins/bin/sdc-jenkins.js
+++ tools/jenkins/sdc-jenkins/bin/sdc-jenkins.js
@@ -3,4 +3,7 @@
 var App = require('../lib/app');
 var cmdln = require('cmdln');
 
+// Avoid DEPTH_ZERO_SELF_SIGNED_CERT error from self-signed certs
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
 cmdln.main(App);
